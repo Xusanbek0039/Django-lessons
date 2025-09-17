68c233d885b0765dd949258f940f5733862d1572
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-09-17 16:54:41 +0500

    Update tests.py

--- Djangoda book list detail test 13-dars/book/tests.py
+++ Djangoda book list detail test 13-dars/book/tests.py
@@ -1,2 +1,17 @@
 from django.test import TestCase
 
+from django.urls import reverse
+from book.models import Book
+
+
+class BookTestCase(TestCase):
+    def test_no_book(self):
+        response = self.client.get(response("books:list"))
+
+        self.assertContains(response, "No books found.")
+
+        
+    def test_books_list(self):
+        pass
+    def test_detail_page(self):
+        pass
