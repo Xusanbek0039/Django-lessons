5558ef8d9abe77e337c48ebbd0e6df6c9ad86153
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-10-03 05:00:09 -0700

    New lessons

--- Djangoda profile yangilash 18-dars/users/models.py
+++ Djangoda profile yangilash 18-dars/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True) # rasm uchun
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True) 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
