e566a18e4f0eb12575ca8598113b960c44a17379
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-10-06 04:56:42 -0700

    New lesons

---
+++ Djangoda UserPicture 20-dars/users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import CustomUser
+
+class UserCreateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)  # <-- email validatsiya bo‘ladi
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "first_name", "last_name", "email", "password")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)  # <-- bazaga yozmasdan obyektni olamiz
+        user.set_password(self.cleaned_data["password"])  # <-- parolni hashlash
+        if commit:
+            user.save()
+        return user
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(max_length=128)
---
+++ Djangoda UserPicture 20-dars/users/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from users.forms import UserCreateForm, UserLoginForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreateForm()
+        return render(request, "users/register.html", {"form": form})
+
+    def post(self, request):
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("users:login")
+        return render(request, "users/register.html", {"form": form})
+
+class LoginView(View):
+    def get(self, request):
+        login_form = UserLoginForm()
+        return render(request, "users/login.html", {"login_form": login_form})
+    
+    def post(self, request):
+        login_form = AuthenticationForm(data=request.POST)
+        if login_form.is_valid():
+            user = login_form.get_user()
+            login(request, user)
+            messages.success(request,"Login qilindi!")
+            return redirect("landing_page")
+        else:
+            return render(request, "users/login.html", {"login_form": login_form})
+        
+class ProfilView(LoginRequiredMixin,View):
+    def get(self,request):
+        return render(request,"users/profil.html", {"user:request":"user"})
+    
+    
+class LogoutView(LoginRequiredMixin,View):
+    def get(self,request):
+        logout(request)
+        messages.info(request,"Tizimdan chiqildi!")
+        return redirect("landing_page")
+
