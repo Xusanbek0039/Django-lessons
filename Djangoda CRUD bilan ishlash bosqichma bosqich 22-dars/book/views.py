1fe6f28a3c9cabacab00786da32082f53e75e7e4
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-10-13 05:27:06 -0700

    Update

--- Djangoda CRUD bilan ishlash bosqichma bosqich 22-dars/book/views.py
+++ Djangoda CRUD bilan ishlash bosqichma bosqich 22-dars/book/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django.views import View
 from book.models import Book
+from django.views.generic import ListView, DetailView
 
-class BooksView(View):
-    def get(self, request):
-        books = Book.objects.all()
-        return render(request, 'books/list.html',{"books": books})
+# CRUD 
+class BooksView(ListView):
+    template_name = 'books/list.html'
+    queryset = Book.objects.all()
+    context_object_name = "books"
+
+
+
+class BookDetailView(DetailView):
+    template_name = 'books/detail.html'
+    pk_url_kwarg = "id"
+    model = Book
+
+
+# class BooksView(View):
+#     def get(self, request):
+#         books = Book.objects.all()
+#         return render(request, 'books/list.html',{"books": books})
     
 
 
-class BookDetailView(View):
-    def get(self,request,id):
-        book=Book.objects.get(id=id)
-        return render(request,'books/detail.html',{"book":book})
+# class BookDetailView(View):
+#     def get(self,request,id):
+#         book= Book.objects.get(id=id)
+#         return render(request,'books/detail.html',{"book":book})
